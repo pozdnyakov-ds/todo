0888ed526a86c08fa31e3cd6ba4453e6752cdfcd
Dmitry Pozdnyakov <2903015@gmail.com>
2022-05-07 20:18:37 +0300

    2.0

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <TodoHeader title="Дела"></TodoHeader>
+  <div>
+    <div style="width: 25%; height: 100%; background-color: antiquewhite;"></div>
+    <div style="width: 75%; height: 100%; background-color: #fff;"></div>
+  </div>
+  
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+
+export default {
+  data() {
+    return {
+      name: 'App',
+      items: [
+        {
+          id: 1,
+          title: 'Заголовок 1',
+          text: 'Текст 1...',
+          state: 0
+        },
+        {
+          id: 2,
+          title: 'Заголовок 2',
+          text: 'Текст 2...',
+          state: 0
+        },
+        {
+          id: 3,
+          title: 'Заголовок 3',
+          text: 'Текст 3...',
+          state: 0
+        },
+      ]
+    }
+  },
+  components: {
+    TodoHeader
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div style="width: 100%; height: 50px; background-color: #eee; padding: 10px; text-align: left; display: flex">
+        <img alt="ToDo logo" src="../assets/logo.png" style="width: 50px; display: inline-block;">
+        <div class="header">{{ title }}</div> 
+        <div class="login">Логин</div>
+    </div>
+</template>
+
+<script>
+    export default {
+          props: ['title'],
+        
+    }
+</script>
+
+<style>
+.header {
+    width: 80%; 
+    margin-left: 10px; 
+    margin: auto;
+    padding: 5px; 
+    display: inline-block;
+    background-color: aquamarine;
+}
+.login {
+    width: 20%; 
+    margin-left: 10px; 
+    margin: auto; 
+    padding: 5px;
+    display: inline-block;
+    background-color: #ccc;
+}
+</style>
